03f900bcd5010736659bfff6211ad3592c398ad1
niyamat <niyamatbarote@gmail.com>
2025-09-26 14:30:08 +0530

    Created Guess The Number Using Oop Concepts

--- JAVA/GuessTheNumberOop.java
+++ JAVA/GuessTheNumberOop.java
@@ -1,32 +1,54 @@
 import java.util.Random;
 import java.util.Scanner;
 
-class Game{
+class Game {
     int guess;
     int random;
+    int attempt = 0;
 
-    Game(){
+    Game() {
         Random rd = new Random();
-        random= rd.nextInt(101);
+        random = rd.nextInt(101);
     }
 
-    public void Guess(){
+    public void Guess() {
+        attempt++;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter You Guess Number : ");
         guess = sc.nextInt();
     }
 
-    boolean isCorrect(){
+    boolean isCorrect() {
         if (guess == random) {
+            System.out.println("Your Guess is Correct : " + random + " Was The Random Number ");
+            System.out.println("You Guess The Number in : " + attempt + " Attempts");
             return true;
+        } else if (guess > random) {
+            System.out.println("Too High.. Guess SMALLER Number");
+        } else if (guess < random) {
+            System.out.println("Too Low .. Guess GREATER Number");
         }
         return false;
     }
 }
 
 public class GuessTheNumberOop {
-    
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        // Object Created :-
+        Game game = new Game();
+
+        boolean b = false;
+        // Game Initializing :-
+        while (!b) {
+
+            game.Guess();
+            b = game.isCorrect();
+            // game.Repeat();
+
+        }
+
     }
+
 }
