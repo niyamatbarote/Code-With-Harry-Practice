37f0fcb9129e2c3594b105c804138fca24b87c4e
niyamat <niyamatbarote@gmail.com>
2025-09-27 23:33:57 +0530

    Created Interface File

---
+++ JAVA/Interfaces.java
@@ -0,0 +1,11 @@
+// Note:- Objects of the interfaces Cannot be Created
+// Note:- References of the interfaces Can be Created
+// Note:- Using Multiple Interfaces We can Crate a Single Class but
+// Using Multiple Abstract class we cannot create Single Class 
+
+
+public class Interfaces {
+    public static void main(String[] args) {
+        
+    }
+}
