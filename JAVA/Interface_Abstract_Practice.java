9eb1d42bd3ac21159184057a2246ece30b92ba03
niyamat <niyamatbarote@gmail.com>
2025-10-01 20:39:05 +0530

    New Class Added

--- JAVA/Interface_Abstract_Practice.java
+++ JAVA/Interface_Abstract_Practice.java
@@ -19,20 +19,58 @@ class SmartPen extends Pen {
         System.out.println("Refilling...");
     }
 }
+// ===================================================================================================
 
+interface BasicAnimal{
+    void Eat();
+    void Sleep();
+}
+
+class Monkey {
+    public void jump(){
+        System.out.println("Im Jumping");
+    }
+    public void Bite(){
+        System.out.println("Im Biting");
+    }
+}
+
+class Human extends Monkey implements BasicAnimal{
+    public void Eat(){
+        System.out.println("Eating...");
+    }
+    public void Sleep(){
+        System.out.println("Sleeping...");
+    }
+
+}
 public class Interface_Abstract_Practice {
     public static void main(String[] args) {
 
         // Create an Abstract Class Pen With Methds Write() and Refill() as Abstract
         // Methods
-        SmartPen sp = new SmartPen();
-        sp.Write();
-        sp.Refill();
+
+        // SmartPen sp = new SmartPen();
+        // sp.Write();
+        // sp.Refill();
 
     // Use the pen Class to Create a Concrete Class "SmartPen" With Additional Method "ChangeNib()"
-        sp.ChangeNib();
 
-    // Create a Class Monkey With jump() & Bite() Methods. Create a Class Human Which Inherits This Monkey Class & implements BasicAnimal interface with Eat() & sleep() Method
-    
+        // sp.ChangeNib();
+
+    // Create a Class Monkey With jump() & Bite() Methods. Create a Class Human Which Inherits This Monkey Class
+    //  & implements BasicAnimal interface with Eat() & sleep() Method
+
+        // Human hm = new Human();
+        // hm.Bite();
+        // hm.Eat();
+        // hm.Sleep();
+        // hm.jump();
+        // Monkey mk = new Human();
+        // mk.Bite();
+        // mk.Sleep();  --> Throws Error
+
+
+// 
     }
 }
