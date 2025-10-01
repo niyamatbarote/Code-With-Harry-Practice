1135c118feba3d2d98b0c38a07b0947ade37c00f
niyamat <niyamatbarote@gmail.com>
2025-10-01 21:04:04 +0530

    Polymorphism Added

--- JAVA/Interface_Abstract_Practice.java
+++ JAVA/Interface_Abstract_Practice.java
@@ -44,21 +44,69 @@ class Human extends Monkey implements BasicAnimal{
     }
 
 }
+
+// ==================================================================================================
+
+interface Timeline{
+    public void SaveTime();
+    public void Timestamp();
+}
+
+interface Record{
+    public void RecordVoice();
+    public void VoiceTimestamp();
+}
+
+abstract class Telephone{
+    abstract public void Ring();
+    abstract public void Answer();
+    abstract public void Decline();
+}
+
+class SmartTelephone extends Telephone implements Timeline,Record{
+
+    public void Ring(){
+        System.out.println("Ringing...");
+    }
+    public void Answer(){
+        System.out.println("Answering The Call ...");
+    }
+    public void Decline(){
+        System.out.println("Declining the Call...");
+    }
+    //Methods of Timeline
+    public void SaveTime(){
+        System.out.println("Saving Timeline");
+    }
+    public void Timestamp(){
+        System.out.println("Saveing TimeStamp");
+    }
+    // Methods of Record
+    public void RecordVoice(){
+        System.out.println("Recording Voice");
+    }
+    public void VoiceTimestamp(){
+        System.out.println("Saving Voice TimeStamp");
+    }
+}
+
+
+
 public class Interface_Abstract_Practice {
     public static void main(String[] args) {
 
-        // Create an Abstract Class Pen With Methds Write() and Refill() as Abstract
+        // Q) Create an Abstract Class Pen With Methds Write() and Refill() as Abstract
         // Methods
 
         // SmartPen sp = new SmartPen();
         // sp.Write();
         // sp.Refill();
 
-    // Use the pen Class to Create a Concrete Class "SmartPen" With Additional Method "ChangeNib()"
+    // Q) Use the pen Class to Create a Concrete Class "SmartPen" With Additional Method "ChangeNib()"
 
         // sp.ChangeNib();
 
-    // Create a Class Monkey With jump() & Bite() Methods. Create a Class Human Which Inherits This Monkey Class
+    // Q) Create a Class Monkey With jump() & Bite() Methods. Create a Class Human Which Inherits This Monkey Class
     //  & implements BasicAnimal interface with Eat() & sleep() Method
 
         // Human hm = new Human();
@@ -71,6 +119,26 @@ public class Interface_Abstract_Practice {
         // mk.Sleep();  --> Throws Error
 
 
-// 
+// Q) Create a Class "Telephone" with Ring(),Answer() & Disconnect() Methods as Abstract MEthods .
+// Create another class SmartTelephone & Demonstrate Polymorphism.
+
+        SmartTelephone stp = new SmartTelephone();
+        stp.Ring();
+        Record rd = new SmartTelephone();
+        Timeline tml = new SmartTelephone();
+
+        rd.RecordVoice();
+        rd.VoiceTimestamp();
+        // rd.TimeStamp();   --> Throws Error Bcoz "Not int the Method of (Record)"
+        // rd.SaveTime();    --> Throws Error Bcoz "Not int the Method of (Record)"
+        tml.Timestamp();
+        tml.SaveTime();
+        // tml.RecordVoice();      --> Throws Error Bcoz "Not int the Method of (Record)"
+        // tml.VoiceTimestamp();   --> Throws Error Bcoz "Not int the Method of (Record)"
+
+
+
+
+
     }
 }
