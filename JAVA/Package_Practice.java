8b614a2c67c2a6836a6ca1141a288f3946313c98
niyamat <niyamatbarote@gmail.com>
2025-10-03 15:47:55 +0530

    PAckage Practice

---
+++ JAVA/Package_Practice.java
@@ -0,0 +1,32 @@
+
+// Create 3 Classes Calclulator , ScCalculator, HybridCalculator & Group them into a Package
+
+package calc.calc1.calc2.calc3;
+
+class Calclulator{
+    public void calclulate(int a, int b){
+        System.out.println("Addition : "+ a+b);
+    }
+}
+class ScCalculator{
+    public void calclulate(int a, int b,int c){
+        System.out.println("Addition: "+ a+b+c);
+    }
+}
+
+class HybCalulator{
+    public void hybCalculate(int a , int b){
+        System.out.println("Multiplication: "+ a*b);
+    }
+}
+
+public class Package_Practice {
+    public static void main(String[] args) {
+        Calclulator simpcal = new Calclulator();
+        ScCalculator sccal = new ScCalculator();
+        HybCalulator hybcal = new HybCalulator();
+
+        simpcal.calclulate(4,6);
+
+`    }
+}
---
+++ JAVA/Package_Practice2.java
@@ -0,0 +1,33 @@
+package calc.calc1.calc2.calc3;
+
+class Calclulator{
+    public void calclulate(int a, int b){
+        System.out.println("Addition : "+ a+b);
+    }
+}
+class ScCalculator{
+    public void calclulate(int a, int b,int c){
+        System.out.println("Addition: "+ a+b+c);
+    }
+}
+
+class HybCalulator{
+    public void hybCalculate(int a , int b){
+        System.out.println("Multiplication: "+ a*b);
+    }
+}
+
+public class Package_Practice2 {
+    public static void main(String[] args) {
+        
+    
+    public static void main(String[] args) {
+        Calclulator simpcal = new Calclulator();
+        ScCalculator sccal = new ScCalculator();
+        HybCalulator hybcal = new HybCalulator();
+
+        simpcal.calclulate(4,6);
+
+`    }
+}
+}
