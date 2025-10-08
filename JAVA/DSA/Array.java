6f210ada12242d1ecfda3c468147cf0971d1008b
niyamat <niyamatbarote@gmail.com>
2025-10-08 16:12:53 +0530

    New Dsa Beginning

--- JAVA/DSA/Array.java
+++ JAVA/DSA/Array.java
@@ -1,7 +1,14 @@
 package DSA;
-
+import java.util.Arrays;
+import java.util.Scanner;
 public class Array {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String[] str = new String[5];
+        System.out.println("Enter The String Value in Array : ");
+        for (int i = 0; i < str.length; i++) {
+            str[i]=sc.next();
+        }
+        System.out.print(Arrays.toString(str));
     }
 }
