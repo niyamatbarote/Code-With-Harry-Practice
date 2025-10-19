a0ac8387c697bb7f7c54994a7943cb6332e6bd15
niyamat <niyamatbarote@gmail.com>
2025-10-19 20:41:19 +0530

    Program to get max from an Array

--- DSA/Array_Swap.java
+++ DSA/Array_Swap.java
@@ -11,12 +11,25 @@ public class Array_Swap {
         arr[IndexB]=temp;
     }
 
+    // Method to Get MAX FRom An Array:-
+    static void getMax(int[] lisy){
+        int max= 0;
+        for (int i = 0; i < lisy.length; i++) {
+            if (max<lisy[i]) {
+                max = lisy[i];
+            }
+        }
+        System.out.println("The Max From This Array is:  "+ max);
+    }
+
     public static void main(String[] args) {
         
-        int[] arr = {1,2,3,4,5};
+        int[] arr = {1,9,3,7,5,10,4};
         swap(arr, 0, 4);
         swap(arr, 1, 3);
         System.out.println(Arrays.toString(arr));
+
+        getMax(arr);
         
     }
 }
