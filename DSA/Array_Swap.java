62f24ad20f901b79bc6c8f8f12495a7f12f6a4d2
niyamat <niyamatbarote@gmail.com>
2025-10-20 18:00:28 +0530

    Reversed The Array

--- DSA/Array_Swap.java
+++ DSA/Array_Swap.java
@@ -1,6 +1,7 @@
 package DSA;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Array_Swap {
 
@@ -22,7 +23,21 @@ public class Array_Swap {
         System.out.println("The Max From This Array is:  "+ max);
     }
 
+    // Method to get MIN From an Array:-
+    static void getMin(int[] arr){
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]<min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("Minimum From This Arrya is: "+min);
+    }
+
+    
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
         int[] arr = {1,9,3,7,5,10,4};
         swap(arr, 0, 4);
@@ -30,6 +45,36 @@ public class Array_Swap {
         System.out.println(Arrays.toString(arr));
 
         getMax(arr);
+
+// ===========================================================================================================//
+
+        // Program to Reverse an Array:-
+
+
+        // Getting Lenght of an array:
+        System.out.println("Enter the length of an Array: ");
+        int n = sc.nextInt();
+
+        int[] myArray = new int[n];
         
+        // Input in Array:-
+        System.out.println("Enter "+n+ " Elements in Array: ");
+        for (int i = 0; i < myArray.length; i++) {
+            myArray[i]=sc.nextInt();
+        }
+        
+        // Printing Array:-
+        System.out.println("Your Array Is: "+Arrays.toString(myArray));
+
+        // Reversing Array:-
+        System.out.println("Reversed Array Would Be: ");
+        for (int i = n-1; i >= 0; i--) {
+            System.out.print(+myArray[i]+" ");
+        }
+
+// ==============================================================================================================
+
+        getMax(myArray);
+        getMin(myArray);
     }
 }
