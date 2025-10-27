1da0697fb1e874c916a7d200f98f83141baeb672
niyamat <niyamatbarote@gmail.com>
2025-10-27 13:47:02 +0530

    Finding Element From an INfinite sorted array

--- DSA/Binary_Search_LC.java
+++ DSA/Binary_Search_LC.java
@@ -101,69 +101,108 @@ public class Binary_Search_LC {
         return arr[start % arr.length];
     }
 
+    // =====================================================================================================================
+
     // Q)34 First and Last Position of an Element in Sorted Array :-
     // https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/?envType=problem-list-v2&envId=vrww4tkr
     static int[] searchRange(int[] arr, int target) {
         int[] ans = new int[2];
-        ans[0] = leftMost(arr,target);
-        ans[1] = rightMost(arr,target);
+        ans[0] = leftMost(arr, target);
+        ans[1] = rightMost(arr, target);
         return ans;
     }
 
-        static int leftMost(int[] a, int target){
-            int start = 0 ;
-            int end = a.length-1;
-            int ans = -1;
+    static int leftMost(int[] a, int target) {
+        int start = 0;
+        int end = a.length - 1;
+        int ans = -1;
 
-            while (start<=end) {
-                int mid = start+(end-start)/2;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
 
-                if (target == a[mid]) {
-                    ans = mid;
-                    end = mid -1;
-                }else if (target < a[mid]) {
-                    end = mid -1;
-                }else{
-                    start = mid +1;
-                }
+            if (target == a[mid]) {
+                ans = mid;
+                end = mid - 1;
+            } else if (target < a[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
-            return ans;
         }
+        return ans;
+    }
 
-        static int rightMost(int[] a, int target) {
-            int start = 0 ;
-            int end = a.length-1;
-            int ans = -1;
+    static int rightMost(int[] a, int target) {
+        int start = 0;
+        int end = a.length - 1;
+        int ans = -1;
 
-            while (start<=end) {
-                int mid = start+(end-start)/2;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
 
-                if (target == a[mid]) {
-                    ans = mid;
-                    start = mid +1;
-                }else if (target < a[mid]) {
-                    end = mid -1;
-                }else{
-                    start = mid +1;
-                }
+            if (target == a[mid]) {
+                ans = mid;
+                start = mid + 1;
+            } else if (target < a[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
-            return ans;
         }
-    
+        return ans;
+    }
+
+    // ==========================================================================================================================
+
+    // Q) Amazon Interview Question :-
+    // Find the element from an INFINITE SORTED ARRAY :-
+
+    static int myAns(int[] arr, int target) {
+
+        int start = 0;
+        int end = 1;
+
+        while (target > arr[end]) {
+            int temp = end + 1; // this is my new start
+            // Double the box value
+            // Formula :- end = end + ArraySize*2
+            end = end + (end - start + 1) * 2;
+            start = temp;
+        }
+        return binarySearch(arr, start, end, target);
+    }
+
+    static int binarySearch(int[] arr, int start, int end, int target) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target == arr[mid]) {
+                return mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    // =====================================================================================================
 
     public static void main(String[] args) {
         char[] arr1 = { 'c', 'f', 'j' };
         int[] arr = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 25, 30, 44, 90 };
         // Index----> 0, 1, 2, 3, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14
-        int[] arr2 = {2,4,7,7,7,7,8,8,9};
+        int[] arr2 = { 2, 4, 7, 7, 7, 7, 8, 8, 9 };
 
         // System.out.println(ceilingNumber(arr, 19));
         // System.out.println(findPeakElement(arr, 91));
         // System.out.println(floorNumber(arr, 1));
         // System.out.println(nextGreatChar(arr1, 'f'));
-        System.out.println(Arrays.toString(searchRange(arr2, 7)));
-        System.out.println(leftMost(arr2, 7));
-        System.out.println(rightMost(arr2, 7));
+        // System.out.println(Arrays.toString(searchRange(arr2, 7)));
+        // System.out.println(leftMost(arr2, 7));
+        // System.out.println(rightMost(arr2, 7));
+        System.out.println(myAns(arr, 90));
 
     }
 }
