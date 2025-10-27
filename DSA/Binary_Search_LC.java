40c792a7dccbe1ff41f4f3a743170b98ca3a0a95
niyamat <niyamatbarote@gmail.com>
2025-10-27 12:08:09 +0530

    Find First & Last Position of THe Element in Sorted Array Q)34

--- DSA/Binary_Search_LC.java
+++ DSA/Binary_Search_LC.java
@@ -1,5 +1,7 @@
 package DSA;
 
+import java.util.Arrays;
+
 public class Binary_Search_LC {
 
     // Ceiling of a Number Problem (Using Binary Search)(Sorted Array):-
@@ -97,17 +99,71 @@ public class Binary_Search_LC {
             }
         }
         return arr[start % arr.length];
-    };
+    }
+
+    // Q)34 First and Last Position of an Element in Sorted Array :-
+    // https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/?envType=problem-list-v2&envId=vrww4tkr
+    static int[] searchRange(int[] arr, int target) {
+        int[] ans = new int[2];
+        ans[0] = leftMost(arr,target);
+        ans[1] = rightMost(arr,target);
+        return ans;
+    }
+
+        static int leftMost(int[] a, int target){
+            int start = 0 ;
+            int end = a.length-1;
+            int ans = -1;
+
+            while (start<=end) {
+                int mid = start+(end-start)/2;
+
+                if (target == a[mid]) {
+                    ans = mid;
+                    end = mid -1;
+                }else if (target < a[mid]) {
+                    end = mid -1;
+                }else{
+                    start = mid +1;
+                }
+            }
+            return ans;
+        }
+
+        static int rightMost(int[] a, int target) {
+            int start = 0 ;
+            int end = a.length-1;
+            int ans = -1;
+
+            while (start<=end) {
+                int mid = start+(end-start)/2;
+
+                if (target == a[mid]) {
+                    ans = mid;
+                    start = mid +1;
+                }else if (target < a[mid]) {
+                    end = mid -1;
+                }else{
+                    start = mid +1;
+                }
+            }
+            return ans;
+        }
+    
 
     public static void main(String[] args) {
         char[] arr1 = { 'c', 'f', 'j' };
         int[] arr = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 25, 30, 44, 90 };
         // Index----> 0, 1, 2, 3, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14
+        int[] arr2 = {2,4,7,7,7,7,8,8,9};
 
         // System.out.println(ceilingNumber(arr, 19));
-        System.out.println(findPeakElement(arr, 91));
-        System.out.println(floorNumber(arr, 1));
-        System.out.println(nextGreatChar(arr1, 'f'));
+        // System.out.println(findPeakElement(arr, 91));
+        // System.out.println(floorNumber(arr, 1));
+        // System.out.println(nextGreatChar(arr1, 'f'));
+        System.out.println(Arrays.toString(searchRange(arr2, 7)));
+        System.out.println(leftMost(arr2, 7));
+        System.out.println(rightMost(arr2, 7));
 
     }
 }
