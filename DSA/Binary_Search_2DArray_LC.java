3f16d56fb58f59b2a4ed9d1d704d5985038a55a3
niyamat <niyamatbarote@gmail.com>
2025-11-01 19:49:51 +0530

    created sorted 2D Array

--- DSA/Binary_Search_2DArray_LC.java
+++ DSA/Binary_Search_2DArray_LC.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Binary_Search_2DArray_LC {
 
-    // Q) Finding elements in Row Sorted & Column Sorted Arrya:-
+    // Q) Finding elements in Row Sorted & Column Sorted Array:-
     // Solution:-
     static int[] searchMatrix(int[][] matrix, int target) {
         int r = 0;
@@ -30,6 +30,12 @@ public class Binary_Search_2DArray_LC {
                 { 15, 25, 35, 45 },
                 { 28, 29, 37, 49 },
                 { 33, 34, 38, 50 }
+        };
+        int[][] mymatrix = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 16},
         };
 
         System.out.println(Arrays.toString(searchMatrix(matrix, 33)));
