9ea5534fd392a370fa008b33e6176629f4cd04af
niyamat <niyamatbarote@gmail.com>
2025-11-02 23:09:30 +0530

    Fully Optimized Bubble Sort

--- DSA/BubbleSort.java
+++ DSA/BubbleSort.java
@@ -12,18 +12,24 @@ public class BubbleSort {
     }
 
     static int[] bubbleSort(int[] array) {
-            for (int i = 0; i < array.length; i++) {
-                for (int j = 1; j < array.length; j++) {
-                    if (array[j-1] > array[j]) {
-                        swap(array, j-1, j);
-                    }
+        boolean swapped;
+        for (int i = 0; i < array.length - 1; i++) {
+            swapped = false;
+            for (int j = 1; j < array.length - i; j++) {
+                if (array[j - 1] > array[j]) {
+                    swap(array, j - 1, j);
+                    swapped = true;
                 }
             }
+            if (!swapped) { // !false = true OR if(swapped == false)
+            break;
+            }
+        }
         return array;
     }
 
     public static void main(String[] args) {
-        int[] arr = { 15, 5, 3, 9, 12, 20, 1, 2 };
+        int[] arr = { 4, 5, 3, 6, 2, 7, 1, 8 };
 
         System.out.println(Arrays.toString(bubbleSort(arr)));
     }
