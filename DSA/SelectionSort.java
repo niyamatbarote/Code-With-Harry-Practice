eabfa2e7feb701b095bb5d190714d7bf688ba918
niyamat <niyamatbarote@gmail.com>
2025-11-03 17:46:51 +0530

    insertion sort

--- DSA/SelectionSort.java
+++ DSA/SelectionSort.java
@@ -12,9 +12,9 @@ public class SelectionSort {
         return arr;
     }
 
-    static int getMax(int[] arr, int start, int end) {
+    static int getMax(int[] arr, int start, int end) {  // finding max in range
         int max = start;
-        for (int i = 0; i < end; i++) {
+        for (int i = 0; i <= end; i++) {
             if (arr[i] > arr[max]) {
                 max = i;
             }
@@ -24,16 +24,16 @@ public class SelectionSort {
 
     static int[] selectionSort(int[] arr) {
 
-        for (int i = 0; i < arr.length; i++) { // FOr Multiple Passes
+        for (int i = 0; i < arr.length; i++) {
             int lastIndex = arr.length - i - 1;
-            int max = getMax(arr, 0, lastIndex);
-            swap(arr, max, lastIndex); // Swapping Max Elements to Last Index
+            int maxItemIndex = getMax(arr, 0, lastIndex);
+            swap(arr, maxItemIndex, lastIndex);
         }
         return arr;
     }
 
     public static void main(String[] args) {
-        int[] arr = { 5, 6, 4, 7, 3, 8, 2, 9, 1 };
+        int[] arr = { 5, 6, 4, 7, 3, 8, 2, 9, 1, 56, 34, 23, 65, 76, 23,1 };
 
         System.out.println(Arrays.toString(selectionSort(arr)));
 
