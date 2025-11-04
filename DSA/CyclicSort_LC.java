02fe01f71bbcd9300cf037a5588754572b3cc892
niyamat <niyamatbarote@gmail.com>
2025-11-04 16:41:10 +0530

    Solved Finding Duplicates & Multiple Duplicates From an Array Using Cyclic Sort

--- DSA/CyclicSort_LC.java
+++ DSA/CyclicSort_LC.java
@@ -6,8 +6,7 @@ import java.util.List;
 
 public class CyclicSort_LC {
 
-    // Amazon Interview Question (Leetcode Q) 258)
-    // https://leetcode.com/problems/missing-number/
+    // Program to SWAP Elements in an Array:
     static int[] swap(int[] arr, int start, int end) {
         int temp = arr[start];
         arr[start] = arr[end];
@@ -15,12 +14,15 @@ public class CyclicSort_LC {
         return arr;
     }
 
+    // Amazon Interview Question (Leetcode Q) 258)
+    // https://leetcode.com/problems/missing-number/
     static int findMissingNum(int[] arr) {
         // int ans = -1;
         int i = 0;
         while (i < arr.length) {
             int correct = arr[i];
-            if (arr[i] < arr.length && arr[i] != arr[correct]) {
+            // (arr[i] < arr.length) this extra condition bcoz range[0,N]...... (Might Be)
+            if (arr[i] < arr.length && arr[i] != arr[correct]) {    
                 swap(arr, i, correct);
             } else {
                 i++;
@@ -29,7 +31,7 @@ public class CyclicSort_LC {
 
         for (int j = 0; j < arr.length; j++) {
             if (arr[j] != j) {
-                return j;   // returning Directly and not storing it, bcoz it maybe overwritten
+                return j; // returning Directly and not storing it, bcoz it maybe overwritten
             }
         }
         return arr.length;
@@ -38,35 +40,80 @@ public class CyclicSort_LC {
     // ===================================================================================================
     // Google Interview Question (LC Q) 448)
     // https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
-    // Note:- if range --> [0,N] , then --> element will be at  index = element
+    // Note:- if range --> [0,N] , then --> element will be at index = element
     // if range --> [1,N], then --> element will be at index = element - 1
     static List<Integer> findAllMissingNum(int[] arr) {
-        int i= 0;
-        while (i<arr.length) {
-            int correct = arr[i]-1;
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
             if (arr[i] != arr[correct]) {
                 swap(arr, i, correct);
-            }else{
+            } else {
                 i++;
             }
         }
-        // check the duplicates:-
-        List<Integer> ans= new ArrayList<>();
+        // check the Missing Number:-
+        List<Integer> ans = new ArrayList<>();
         for (int j = 0; j < arr.length; j++) {
-            if (arr[j] != j+1) {
-                ans.add(j+1);
+            if (arr[j] != j + 1) {
+                ans.add(j + 1);
             }
         }
         return ans;
     }
-// ===============================================================================================
-// Amazon Interview Qusn(Lc Q) 287)
-// https://leetcode.com/problems/find-the-duplicate-number/
+
+    // ===============================================================================================
+    // Amazon Interview Que(Lc Q) 287)
+    // https://leetcode.com/problems/find-the-duplicate-number/
+    static int findDuplicates(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+                // return i;
+            } else {
+                i++;
+            }
+        }
+        // Check for Duplicates
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] != j + 1) {
+                return arr[j];
+            }
+        }
+        return -1;
+    }
+
+    // Microsoft Que (LC Q) 442)
+    // https://leetcode.com/problems/find-all-duplicates-in-an-array/
+    static List<Integer> findMultiDuplicates(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+        // Condition to Check The Duplicates
+        List<Integer> ans = new ArrayList<>();
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] != j + 1) {
+                ans.add(arr[j]);
+            }
+        }
+        return ans;
+    }
 
     public static void main(String[] args) {
         int[] arr = { 2, 0, 3, 4, 5, 6, 8, 1 };
-        int[] array = {4,5,1,6,9,7,8};
-        
+        int[] array = { 4, 5, 1, 6, 9, 7, 8 };
+        int[] duplicate = { 4, 5, 3, 2, 3, 1 };
+
         // System.out.println(findMissingNum(arr));
+        // System.out.println(findDuplicates(duplicate));
+        // System.out.println(findMultiDuplicates(duplicate)); 
     }
 }
