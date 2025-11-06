08ed3e688217f3340ddf285ef815e09a7a120159
niyamat <niyamatbarote@gmail.com>
2025-11-06 15:42:18 +0530

    New Beginning to Advanced Java

---
+++ Advanced_JAVA/CollectionFrameworks.java
@@ -0,0 +1,16 @@
+package Advanced_JAVA;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CollectionFrameworks {
+
+    public static void main(String[] args) {
+        
+        // ArrayList
+        // TreeSet
+        // Set
+
+    }
+}
