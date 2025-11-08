7b61b0f447f8252db8979bf2610ed78703c9d9d4
niyamat <niyamatbarote@gmail.com>
2025-11-08 11:56:48 +0530

    Done Some EZ Patterns

--- DSA/Pattern.java
+++ DSA/Pattern.java
@@ -26,6 +26,28 @@ public class Pattern {
             System.out.println();
         }
 
-        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i); // Here (i)
+            }
+            System.out.println();
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j); // Here (j)
+            }
+            System.out.println();
+        }
+
+        // for (int i = 0; i <= n; i++) {
+        //     for (int j = 4; j >= (n / 2) + 1; j--) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = n / 2; j <= i ; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
     }
 }
