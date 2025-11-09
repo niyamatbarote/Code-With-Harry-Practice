3dc1f52d50cdd4c030ab6325952c0b2dfa35dfef
niyamat <niyamatbarote@gmail.com>
2025-11-09 22:15:22 +0530

    Date & Time

---
+++ DSA/Date_And_Time.java
@@ -0,0 +1,11 @@
+package DSA;
+
+public class Date_And_Time {
+    public static void main(String[] args) {
+        
+        System.out.println("How Many Years Passed Till Now Since Java is Calculating Time? \nAnswer is : ");
+        System.out.println(System.currentTimeMillis()/1000/3600/24/365);
+        System.out.println("So, Java is Calculating time since : "+ (2025 - 55));
+        
+    }
+}
