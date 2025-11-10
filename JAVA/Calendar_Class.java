99ea0ea19819ac79810fe2f427f98dc4b3cb86d6
niyamat <niyamatbarote@gmail.com>
2025-11-10 22:33:15 +0530

    Calendar Class

---
+++ JAVA/Calendar_Class.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Calendar_Class {
+    public static void main(String[] args) {
+
+        Calendar cal = Calendar.getInstance();
+        System.out.println(cal.getTime());
+        System.out.println(cal.getTimeZone());
+        System.out.println(cal.getTimeZone().getID());
+        // System.out.println(cal.getTimeZone().getDefault());
+        System.out.println(cal.getTimeInMillis());
+        System.out.println(cal.getTimeZone().toZoneId());
+        System.out.println(cal.getCalendarType());
+        System.out.println(cal.getWeekYear());
+    }
+}
