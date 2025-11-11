e7978ab3af204b10d8905ebaab35e25d845cfed0
niyamat <niyamatbarote@gmail.com>
2025-11-11 18:12:02 +0530

    DateTimeFormatter

---
+++ JAVA/DateTimeFormatters.java
@@ -0,0 +1,24 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class DateTimeFormatters {
+    public static void main(String[] args) {
+
+        LocalDateTime dt = LocalDateTime.now();
+
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/mm/yyyy");
+        DateTimeFormatter df1 = DateTimeFormatter.ofPattern("dd-mm-yy");
+        DateTimeFormatter df2 = DateTimeFormatter.ofPattern("**||dd:mm:yyyy||**asd");
+
+        System.out.println(dt);
+        String myDate = dt.format(df);
+        String myDate1 = dt.format(df1);
+        String myDate2 = dt.format(df2);
+
+        System.out.println(myDate);
+        System.out.println(myDate1);
+        System.out.println(myDate2);
+
+    }
+}
