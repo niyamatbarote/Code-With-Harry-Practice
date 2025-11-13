861706662e70c0470c3ca90c2dedc39960dc9a27
niyamat <niyamatbarote@gmail.com>
2025-11-13 17:36:48 +0530

    PreparedStatements

--- JDBC/MyJDBC/src/MyJDBC.java
+++ JDBC/MyJDBC/src/MyJDBC.java
@@ -1,6 +1,7 @@
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -14,35 +15,44 @@ public class MyJDBC {
     public static void main(String[] args) {
 
         try {
-            // Load & Register Driver
+            // 1) Load & Register Driver
             Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // 2) Connection Establishing
+            Connection con = DriverManager.getConnection(url, username, password);
             System.out.println("Conncection Established..!!");
-        } catch (ClassNotFoundException e) {
-            System.out.println(e);
-        }
 
-        try {
-            // Establish Connection
-            Connection connect = DriverManager.getConnection(url, username, password);
-            Statement state = connect.createStatement();
-            String query = "select * from patients";
-            // Here, ReslutSet Fetches the Data From the Database in SQL.
-            ResultSet resultset = state.executeQuery(query);
+            // 3a) Statement Creating --
+            String query = "Select pid, name, age from patients where pid=?";
+            // Statement st = con.createStatement(); // We Are not using it here
 
-            while (resultset.next()) {// to run program till last row
+            // 3b) PreparedStatement Creation (Advanced & Most Used)
+            PreparedStatement pst = con.prepareStatement(query);
 
-                // result.getInt is used to fetch column ** we need to put the correct Column
-                // name
-                int pid = resultset.getInt("pid");
-                String name = resultset.getString("Name");
-                int age = resultset.getInt("Age");
-                System.out.println("ID : " + pid);
-                System.out.println("NAME : " + name);
-                System.out.println("AGE : " + age);
+            // 4) Filling Value
+            pst.setInt(1, 3);
+
+            // 5) Resultset
+            ResultSet rs = pst.executeQuery();
+
+            // 6) Executing Queries
+            while (rs.next()) {
+                System.out.println(rs.getInt("pid") + " " + rs.getString("name") + " " + rs.getInt("age"));
 
             }
-        } catch (SQLException e) {
+
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
 }
+
+// This is the method fetching data using statements -->
+// // result.getInt is used to fetch column ** we need to put the correct Column
+// // name
+// int pid = resultset.getInt("pid");
+// String name = resultset.getString("Name");
+// int age = resultset.getInt("Age");
+// System.out.println("ID : " + pid);
+// System.out.println("NAME : " + name);
+// System.out.println("AGE : " + age);
