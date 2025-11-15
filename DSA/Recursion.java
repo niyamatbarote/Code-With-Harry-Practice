b5115e0118ff7697c0a01e5847927ea0a7c8e50d
niyamat <niyamatbarote@gmail.com>
2025-11-15 15:19:21 +0530

    Binary Search Using Recursion

--- DSA/Recursion.java
+++ DSA/Recursion.java
@@ -2,6 +2,29 @@ package DSA;
 
 public class Recursion {
 
+    // Binary Search using recursion
+    static int binarySearch(int[] arr, int target, int start, int end) {
+
+        // Base Case :-
+        if (start > end) {
+            return -1;
+        }
+
+        int mid = start + (end - start) / 2;
+
+        // Recursive Steps :-
+        if (target == arr[mid]) {
+            return mid;
+        }
+
+        if (target > arr[mid]) {
+            return binarySearch(arr, target, mid + 1, end);
+        }
+
+        return binarySearch(arr, target, start, mid - 1);
+
+    }
+
     static int fibonacci(int n) {
 
         // Base Case :-
@@ -23,8 +46,22 @@ public class Recursion {
     }
 
     public static void main(String[] args) {
-        System.out.println(factorial(5));
-        System.out.println(fibonacci(3));
+
+        int[] arr = { 12, 23, 34, 45, 56, 67, 78, 89, 90 };
+
+        // Following are non-tail recurson as they are waiting for other values to
+        // complete the calcultn
+        System.out.println(factorial(4));
+
+        // Here Due to Larger Nth value of fibonacci, the program got stuck in the tree
+        // computing the repititive calls, and it stuck bcoz of long and large tree
+        // components
+        // System.out.println(fibonacci(50));
+
+        // sol of BS:-
+        int target = 12 ;
+        int ans = binarySearch(arr, target, 0, arr.length - 1);
+        System.out.println(ans);
 
     }
 }
