c29420676142bfc5e56dca2bf3527faad94c1940
niyamat <niyamatbarote@gmail.com>
2025-11-16 15:26:18 +0530

    Finding Unique || Non Duplicate element from array

--- DSA/Maths.java
+++ DSA/Maths.java
@@ -8,9 +8,24 @@ public class Maths {
         return (n & 1) == 1;
     }
 
+    // To Find The Unique Element from an Arrya, where all the other elements in
+    // that array are duplicates
+    public static int findUnique(int[] arr) {
+        int unique = 0;
+
+        for (int elements : arr) {
+            unique ^= elements;
+        }
+        return unique;
+    }
+
     public static void main(String[] args) {
+
+        int arr[] = { 1, -2, 3, 4, -2, 1, 3 };
 
         System.out.println(isOdd(55));
         System.out.println(isOdd(20));
+        System.out.println(findUnique(arr));
+
     }
 }
