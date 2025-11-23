9297b0f6f9a9fd77d2fb0312d884d0ae3a0c992a
niyamat <niyamatbarote@gmail.com>
2025-11-23 16:37:52 +0530

    idk

--- MavenProject2/maven_parent/child1/src/main/java/com/parent/Main.java
+++ MavenProject2/maven_parent/child1/src/main/java/com/parent/Main.java
@@ -1,7 +1,13 @@
 package com.parent;
 
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        Gson gson = new Gson();
+        System.out.println(gson.toJson(Map.of("Message", "Working Fine: child1")));
+
     }
-}
+}   
